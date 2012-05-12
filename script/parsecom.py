dc68948dc381c7ba73388d0b01ed0f45fdbb5d54
Varrun Ramani <varrunr@gmail.com>
2012-05-12 06:22:27 -0400

    Script to parse .com database in search for Homographs

---
+++ script/parsecom.py
@@ -0,0 +1,50 @@
+# coding: utf-8
+import codecs
+
+def create_confusables():
+	ConfusableMatrix = [
+						['ə' , 'ә'],
+						['а' , 'a'],
+						['o' , 'ο' , 'о'],
+						['s' , 'ѕ'],
+						['x' , 'х'],
+						['æ' , 'ӕ'],
+						['i' , 'і'],
+						['j' , 'ј'],
+						['p' , 'р'],
+						['c' , 'с'],
+						['y' , 'у'],
+						['a' , 'ɑ'],
+						['ɩ','l','1'],
+						['ł' , 'ɫ'],
+						['g','ɡ'],
+						['v','ѵ','ν'],
+						['b', 'ƅ'],
+						['m','rn'],
+						['p','ρ','р'],
+						['r','г'],
+						['n','ո'],
+						['h','հ'],
+						['u','ս'],
+						['f','ք'],
+						['y','ყ'],
+						['-','‐','‒']									
+			   		];
+	return ConfusableMatrix;
+
+def read_file(filename):
+	f_intdom = open(filename,'r')
+	f_unidom = codecs.open('UNIDOM', 'w', encoding='utf-16')
+	for domain in f_intdom:
+		try:
+			domain = domain[0:len(domain) - 1].lower().encode('ascii')
+			uniUrl = codecs.lookup('idna').decode(domain)[0]
+			f_unidom.write(uniUrl+'\n')
+		except UnicodeError:
+			print "Unicode Error"
+
+	f_intdom.close()
+	f_unidom.close()
+		
+confusables = create_confusables()
+read_file('inp')
